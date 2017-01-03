c69072e5c5bdfa7486b86dc69d9cf5bc297abcf5
Jorge Otero <oterogalvis@gmail.com>
2017-01-03 10:14:02 -0500

    Committing notes until January 3rd, 2017

---
+++ java/Example_08_splittingStrings.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+
+class Example_08_splittingStrings {
+	public static void main(String[] args) {
+		String example = "how do we get each of these words";
+		String[] example_split = example.split(" ");
+		// String[] example_split = ["how", "do", "we", "get", "each", "of", "these", "words"]
+	}	
+}

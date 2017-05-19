0fcc69d507a236bcfc46595623ffe5f9bf6260cc
Jorge Otero <oterogalvis@gmail.com>
2017-05-18 22:46:50 -0400

    Committing notes until May 18th, 2017.

---
+++ java/src/example15/com/treehouse/docgen/Doc.java
@@ -0,0 +1,12 @@
+package example15.com.treehouse.docgen;
+
+/**
+ * Created by jorgeotero on 5/18/17.
+ */
+public @interface Doc {
+    /** Description of class or method */
+
+    /** Description of parameters, if annotated element is a method & has parameters */
+
+    /** Description of return value, if annotated element is a method & isn't void */
+}
---
+++ java/src/example15/com/treehouse/docgen/Main.java
@@ -0,0 +1,10 @@
+package example15.com.treehouse.docgen;
+
+/**
+ * Created by jorgeotero on 5/18/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Process the MathUtils class's annotations
+    }
+}
